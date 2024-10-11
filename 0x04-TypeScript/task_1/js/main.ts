a28ca2eb1617d944828c17c349c1901b7a66913b
Lamia Hamdi <lamia.hamdi1406@gmail.com>
2024-10-11 19:11:00 +0100

    create class with interfaces for constructor, class and methods

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,5 +16,29 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 };
-  
-  
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
